ab4d9fbb68dcb235b29aafe0df3d6335e2c81a0b
KerCrafter <clemkerneur@gmail.com>
2026-03-26 17:41:04 +0100

    refactor, generify check_LED

--- tests/test.py
+++ tests/test.py
@@ -6,9 +6,12 @@ from cocotb.triggers import ClockCycles
 async def should_start_with_black_screen(dut):
     dut._log.info("Start")
 
+    check_LED(dut, led_number=0, red=0, green=0, blue=0);
 
-    dut.current_led.value = 0
 
-    assert dut.led_red_intensity.value == 0
-    assert dut.led_green_intensity.value == 0
-    assert dut.led_blue_intensity.value == 0
+def check_LED(dut, led_number, red, green, blue):
+    dut.current_led.value = led_number
+
+    assert dut.led_red_intensity.value == red 
+    assert dut.led_green_intensity.value == green 
+    assert dut.led_blue_intensity.value == blue 
