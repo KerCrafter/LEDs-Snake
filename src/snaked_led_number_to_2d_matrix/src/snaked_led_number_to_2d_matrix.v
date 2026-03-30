875384f3b20c93393af865f16313874d767d9fe2
KerCrafter <clemkerneur@gmail.com>
2026-03-30 11:07:53 +0200

    refacto renaming

--- src/snaked_led_number_to_2d_matrix/src/snaked_led_number_to_2d_matrix.v
+++ src/snaked_led_number_to_2d_matrix/src/snaked_led_number_to_2d_matrix.v
@@ -6,19 +6,21 @@ module snaked_led_number_to_2d_matrix (
   output reg [3:0] y
 );
 
-  reg [4:0] yy;
+  reg [3:0] line_num;
   reg is_pair;
 
   always @(*) begin
-    yy = n / 16;
-    is_pair = yy == 0 || yy == 2 || yy == 4;
-    y <= yy;
+    line_num = n / 16;
+    is_pair = line_num == 0 || line_num == 2 || line_num == 4;
 
     if(is_pair) begin
       x <= 15 - n;
     end else begin
       x <= n - 16;
     end
+
+    y <= line_num;
+
   end
 
 endmodule
