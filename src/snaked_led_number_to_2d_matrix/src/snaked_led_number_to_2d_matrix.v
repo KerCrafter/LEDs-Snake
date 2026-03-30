f25d7ef22d74cfe326806c62f11516d2e5ee324c
KerCrafter <clemkerneur@gmail.com>
2026-03-30 10:35:53 +0200

    refacto generify

--- src/snaked_led_number_to_2d_matrix/src/snaked_led_number_to_2d_matrix.v
+++ src/snaked_led_number_to_2d_matrix/src/snaked_led_number_to_2d_matrix.v
@@ -6,20 +6,19 @@ module snaked_led_number_to_2d_matrix (
   output reg [3:0] y
 );
 
+  reg [4:0] yy;
+
   always @(*) begin
-    if(n >= 0 && n <= 15) begin
-      x <= 15 - n;
-      y <= 0;
-    end
 
-    if(n >= 16 && n <= 31) begin
+    yy = n / 16;
+    y <= yy;
+
+    if(yy == 1) begin
       x <= n - 16;
-      y <= 1;
     end
 
-    if(n >= 32 && n <= 47) begin
-      x <= 47 - n;
-      y <= 2;
+    if(yy == 0 || yy == 2) begin
+      x <= (yy * 16) + 15 - n;
     end
   end
 
