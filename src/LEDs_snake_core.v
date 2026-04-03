953ceb83a6bc95092da18c0e42c609851b68f0e0
KerCrafter <clemkerneur@gmail.com>
2026-04-03 17:27:10 +0200

    draw bonus pipeline

--- src/LEDs_snake_core.v
+++ src/LEDs_snake_core.v
@@ -1,3 +1,30 @@
+module DrawBonus (
+  input wire [3:0] current_led_x,
+  input wire [3:0] current_led_y,
+  input wire [3:0] bonus_x_pos,
+  input wire [3:0] bonus_y_pos,
+  input wire [7:0] led_red_intensity_in,
+  input wire [7:0] led_blue_intensity_in,
+  input wire [7:0] led_green_intensity_in,
+  output reg [7:0] led_red_intensity_out,
+  output reg [7:0] led_blue_intensity_out,
+  output reg [7:0] led_green_intensity_out
+);
+
+  always @(*) begin
+    if(current_led_x == bonus_x_pos && current_led_y == bonus_y_pos) begin
+      led_red_intensity_out <= 10;
+      led_green_intensity_out <= 0;
+      led_blue_intensity_out <= 0;
+    end else begin
+      led_red_intensity_out <= led_red_intensity_in;
+      led_green_intensity_out <= led_green_intensity_in;
+      led_blue_intensity_out <= led_blue_intensity_in;
+    end
+  end
+
+endmodule
+
 module DrawEndGame (
   input wire end_game,
   input wire [7:0] led_red_intensity_in,
@@ -146,53 +173,61 @@ module LEDs_snake_core (
     .is_head_collide(queue_last_collide)
   );
 
+  DrawBonus draw_bonus (
+    .current_led_x(current_led_x),
+    .current_led_y(current_led_y),
+    .bonus_x_pos(current_bonus_x_pos),
+    .bonus_y_pos(current_bonus_y_pos),
+    .led_red_intensity_in(0),
+    .led_green_intensity_in(0),
+    .led_blue_intensity_in(0),
+    .led_red_intensity_out(led_red_intensity_1),
+    .led_green_intensity_out(led_green_intensity_1),
+    .led_blue_intensity_out(led_blue_intensity_1)
+  );
+
   DrawEndGame draw_end_game (
     .end_game(queue_last_collide),
-    .led_red_intensity_in(led_red_intensity_1),
-    .led_green_intensity_in(led_green_intensity_1),
-    .led_blue_intensity_in(led_blue_intensity_1),
+    .led_red_intensity_in(led_red_intensity_2),
+    .led_green_intensity_in(led_green_intensity_2),
+    .led_blue_intensity_in(led_blue_intensity_2),
     .led_red_intensity_out(led_red_intensity),
     .led_green_intensity_out(led_green_intensity),
     .led_blue_intensity_out(led_blue_intensity)
   );
 
+
   reg [7:0] led_red_intensity_1;
   reg [7:0] led_green_intensity_1;
   reg [7:0] led_blue_intensity_1;
 
+  reg [7:0] led_red_intensity_2;
+  reg [7:0] led_green_intensity_2;
+  reg [7:0] led_blue_intensity_2;
+
   assign end_game = queue_last_collide;
 
-  always @(clk) begin
-    if(reset) begin
-      led_red_intensity_1 <= 0;
-      led_green_intensity_1 <= 0;
-      led_blue_intensity_1 <= 0;
+  always @(*) begin
+    if(score >= 1 && current_led_x == queue_1_x && current_led_y == queue_1_y) begin
+      led_red_intensity_2 <= 0;
+      led_green_intensity_2 <= 5;
+      led_blue_intensity_2 <= 0;
+    end else if(score >= 2 && current_led_x == queue_2_x && current_led_y == queue_2_y) begin
+      led_red_intensity_2 <= 0;
+      led_green_intensity_2 <= 5;
+      led_blue_intensity_2 <= 0;
+    end else if(score >= 3 && current_led_x == queue_3_x && current_led_y == queue_3_y) begin
+      led_red_intensity_2 <= 0;
+      led_green_intensity_2 <= 5;
+      led_blue_intensity_2 <= 0;
+    end else if(current_led_x == snake_head_x_pos && current_led_y == snake_head_y_pos) begin
+      led_red_intensity_2 <= 0;
+      led_green_intensity_2 <= 10;
+      led_blue_intensity_2 <= 0;
     end else begin
-      if(score >= 1 && current_led_x == queue_1_x && current_led_y == queue_1_y) begin
-        led_red_intensity_1 <= 0;
-        led_green_intensity_1 <= 5;
-        led_blue_intensity_1 <= 0;
-      end else if(score >= 2 && current_led_x == queue_2_x && current_led_y == queue_2_y) begin
-        led_red_intensity_1 <= 0;
-        led_green_intensity_1 <= 5;
-        led_blue_intensity_1 <= 0;
-      end else if(score >= 3 && current_led_x == queue_3_x && current_led_y == queue_3_y) begin
-        led_red_intensity_1 <= 0;
-        led_green_intensity_1 <= 5;
-        led_blue_intensity_1 <= 0;
-      end else if(current_led_x == snake_head_x_pos && current_led_y == snake_head_y_pos) begin
-        led_red_intensity_1 <= 0;
-        led_green_intensity_1 <= 10;
-        led_blue_intensity_1 <= 0;
-      end else if(current_led_x == current_bonus_x_pos && current_led_y == current_bonus_y_pos) begin
-        led_red_intensity_1 <= 10;
-        led_green_intensity_1 <= 0;
-        led_blue_intensity_1 <= 0;
-      end else begin
-        led_red_intensity_1 <= 0;
-        led_green_intensity_1 <= 0;
-        led_blue_intensity_1 <= 0;
-      end
+      led_red_intensity_2 <= led_red_intensity_1;
+      led_green_intensity_2 <= led_green_intensity_1;
+      led_blue_intensity_2 <= led_blue_intensity_1;
     end
   end
 
