536cb224fbdfc1fe2419b903f4a4ac874c914cb0
KerCrafter <clemkerneur@gmail.com>
2026-04-04 23:20:28 +0200

    dynamic active score for queue

--- src/snake_queue.v
+++ src/snake_queue.v
@@ -9,6 +9,7 @@ module SnakeQueue (
   input wire [3:0] head_x_pos,
   input wire [3:0] head_y_pos,
   input wire next_collide_head,
+  input wire [7:0] active_when_score_min,
   output reg [3:0] x_pos,
   output reg [3:0] y_pos,
   output reg is_head_collide
@@ -22,7 +23,7 @@ module SnakeQueue (
       next_collide_head ||
       (direction == 0 && head_x_pos + 1 == x_pos && head_y_pos == y_pos) || 
       (direction == 1 && head_x_pos - 1 == x_pos && head_y_pos == y_pos) ||
-      (direction == 2 && score >= 2 && head_y_pos + 1 == y_pos && head_x_pos == x_pos) ||
+      (direction == 2 && score >= active_when_score_min && head_y_pos + 1 == y_pos && head_x_pos == x_pos) ||
       (direction == 3 && head_y_pos - 1 == y_pos && head_x_pos == x_pos) 
     ) begin
       is_head_collide <= 1; 
