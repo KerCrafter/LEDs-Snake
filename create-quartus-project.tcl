8087fc60f0f40d6ab8c108fc9e2c583b841b493c
KerCrafter <clemkerneur@gmail.com>
2026-04-05 23:24:54 +0200

    automatic resolve import files from creact-quartus-project.tcl file

--- create-quartus-project.tcl
+++ create-quartus-project.tcl
@@ -8,12 +8,19 @@ set_global_assignment -name PROJECT_OUTPUT_DIRECTORY output_files
 set_global_assignment -name VERILOG_FILE src/devices_entry/DE0_NANO_LEDs_snake_main.v
 set_global_assignment -name VERILOG_FILE src/LEDs_snake_main.v 
 set_global_assignment -name VERILOG_FILE src/LEDs_snake_core.v
-
 set_global_assignment -name VERILOG_FILE src/button-debouncer/button_debouncer.v
 set_global_assignment -name VERILOG_FILE src/WS2812B-driver/WS2812B_driver.v
 set_global_assignment -name VERILOG_FILE src/WS2812B-driver/NRZ_sequence.v
 set_global_assignment -name VERILOG_FILE src/WS2812B-driver/pipe_tri_bus.v
 set_global_assignment -name VERILOG_FILE src/snaked_led_number_to_2d_matrix/src/snaked_led_number_to_2d_matrix.v
+set_global_assignment -name VERILOG_FILE src/bonus_manager.v
+set_global_assignment -name VERILOG_FILE src/player_commands_manager.v
+set_global_assignment -name VERILOG_FILE src/snake_head.v
+set_global_assignment -name VERILOG_FILE src/snake_queue.v
+set_global_assignment -name VERILOG_FILE src/draw_bonus.v
+set_global_assignment -name VERILOG_FILE src/draw_end_game.v
+set_global_assignment -name VERILOG_FILE src/draw_snake_head.v
+set_global_assignment -name VERILOG_FILE src/draw_snake_queue.v
 
 set_location_assignment PIN_R8 -to clk
 set_location_assignment PIN_T14 -to leds_line
--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
+export PROJECT_SOURCES=`grep "set_global_assignment -name VERILOG_FILE" create-quartus-project.tcl | awk '{print $4}' | xargs` 
 
-export PROJECT_SOURCES="LEDs_snake_main.v LEDs_snake_core.v bonus_manager.v player_commands_manager.v snake_head.v snake_queue.v draw_snake_queue.v draw_snake_head.v draw_bonus.v draw_end_game.v"
 ./tests/cocotb-runner/run_tests.sh
