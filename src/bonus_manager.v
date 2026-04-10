106c165efc2770bc3b8fb4ccd6061dc94e64b5d5
KerCrafter <clemkerneur@gmail.com>
2026-04-10 17:10:00 +0200

    refacto naming

--- src/bonus_manager.v
+++ src/bonus_manager.v
@@ -3,8 +3,8 @@ module BonusManager (
   input wire clk,
   input wire reset,
   input wire [1:0] head_direction,
-  input wire [3:0] snake_head_x_pos,
-  input wire [3:0] snake_head_y_pos,
+  input wire [3:0] head_x_pos,
+  input wire [3:0] head_y_pos,
   input wire [3:0] bonus_random_x,
   input wire [3:0] bonus_random_y,
   output reg [3:0] x_pos,
@@ -17,10 +17,10 @@ module BonusManager (
       x_pos <= bonus_random_x;
       y_pos <= bonus_random_y;
     end else if(move_act && (
-      (head_direction == 0 && snake_head_x_pos + 1 == x_pos && snake_head_y_pos == y_pos) ||
-      (head_direction == 1 && snake_head_x_pos - 1 == x_pos && snake_head_y_pos == y_pos) ||
-      (head_direction == 2 && snake_head_y_pos + 1 == y_pos && snake_head_x_pos == x_pos) ||
-      (head_direction == 3 && snake_head_y_pos - 1 == y_pos && snake_head_x_pos == x_pos)
+      (head_direction == 0 && head_x_pos + 1 == x_pos && head_y_pos == y_pos) ||
+      (head_direction == 1 && head_x_pos - 1 == x_pos && head_y_pos == y_pos) ||
+      (head_direction == 2 && head_y_pos + 1 == y_pos && head_x_pos == x_pos) ||
+      (head_direction == 3 && head_y_pos - 1 == y_pos && head_x_pos == x_pos)
     )) begin
       score <= score + 8'd1;
 
